7709bed684aa8fe3a6448acb05edd0a753a81664
khanshhh <137532572+Khanshh@users.noreply.github.com>
2026-05-20 14:26:51 +0700

    [FIX] Lỗi API

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,6 @@
 # Export all models here so Alembic can detect them automatically
 from app.models.user import User, UserRole
 from app.models.refresh_token import RefreshToken
+from app.models.pet import Pet
 
-__all__ = ["User", "UserRole", "RefreshToken"]
+__all__ = ["User", "UserRole", "RefreshToken", "Pet"]
