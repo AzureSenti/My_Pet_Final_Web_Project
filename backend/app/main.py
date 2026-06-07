a64443eff88062cccea56e24706006ef265ac920
khanshhh <137532572+Khanshh@users.noreply.github.com>
2026-06-07 13:21:17 +0700

    [FIX]main

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,7 +52,7 @@ if netlify_url:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=["*"],  # Cho phép tất cả mọi nguồn để tránh lỗi CORS khi deploy
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
